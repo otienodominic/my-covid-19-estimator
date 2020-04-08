680511eb2876d41314454039f8169338bd7e9881
Dominic Ngalo <otiendominic@gmail.com>
2020-04-08 14:09:58 +0300

    Challenge One

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,15 @@
-const covid19ImpactEstimator = (data) => data;
+import { input } from '../data';
+
+const covid19ImpactEstimator = (data) => ({
+  data: input,
+  impact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: data.currentlyInfected * 1024
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: this.currentlyInfected * 1024
+  }
+});
 
 export default covid19ImpactEstimator;
