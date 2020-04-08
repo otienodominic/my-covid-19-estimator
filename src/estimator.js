32a317c7e644df792ff2d2f42953400257d7bc27
Dominic Ngalo <otiendominic@gmail.com>
2020-04-08 18:34:22 +0300

    Challenge One #3

--- src/estimator.js
+++ src/estimator.js
@@ -1,15 +1,16 @@
-import { input } from '../data/index';
-
-const covid19ImpactEstimator = (data) => ({
-  data: input,
-  impact: {
-    currentlyInfected: data.reportedCases * 10,
-    infectionsByRequestedTime: data.currentlyInfected * 1024
-  },
-  severeImpact: {
-    currentlyInfected: data.reportedCases * 50,
-    infectionsByRequestedTime: this.currentlyInfected * 1024
-  }
-});
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: data.reportedCases * 10,
+      infectionsByRequestedTime: data.currentlyInfected * 1024
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCases * 50,
+      infectionsByRequestedTime: this.currentlyInfected * 1024
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
