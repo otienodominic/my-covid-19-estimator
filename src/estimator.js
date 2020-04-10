a6382034c3bbb4b04dae6e2167957747c5c31234
Dominic Ngalo <otiendominic@gmail.com>
2020-04-10 15:02:07 +0300

    fixed challenge three

--- src/estimator.js
+++ src/estimator.js
@@ -36,8 +36,8 @@ const covid19ImpactEstimator = (data) => {
   const requireICUone = Math.trunc(0.05 * infectedBytimeOne);
   const requireICUtwo = Math.trunc(0.05 * infectedBytimeTwo);
   // Impact on Ventilators
-  const requireVentilatorOne = Math.trunc(0.02 * infectedBytimeOne);
-  const requireVentilatorTwo = Math.trunc(0.02 * infectedBytimeTwo);
+  const requireVentilatorOne = (infectedBytimeOne * 0.02);
+  const requireVentilatorTwo = (infectedBytimeTwo * 0.02);
   // eslint-disable-next-line max-len
   const dollarsLostOne = (infectedBytimeOne * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD * period);
   // eslint-disable-next-line max-len
