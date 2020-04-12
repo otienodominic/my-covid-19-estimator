0a74a5102523c936707b6a63c67e4b2f2917815c
Dominic Ngalo <otiendominic@gmail.com>
2020-04-12 14:16:28 +0300

    updated the estimator

--- src/estimator.js
+++ src/estimator.js
@@ -2,34 +2,37 @@ const covid19ImpactEstimator = (data) => {
   // Normalise the time in days, weeks and months
   // eslint-disable-next-line no-shadow
   const normalisedPeriod = (data) => {
-    const period = data.periodType;
-    const timeElapsed = data.timeToElapse;
-    const elapsedTime = timeElapsed.toString().toLowerCase();
-    switch (period) {
+    const duration = data.periodType.toString().toLowerCase();
+    const time = data.timeToElapse;
+    switch (duration) {
       case 'days':
-        return elapsedTime;
+        return time;
       case 'weeks':
-        return elapsedTime * 7;
+        return time * 7;
       case 'months':
-        return elapsedTime * 30;
+        return time * 30;
       default:
         break;
     }
-    return data;
+    return data.timeToElapse;
   };
   // Challenge One:
   const input = data;
   const period = normalisedPeriod(input);
   const infectedOne = input.reportedCases * 10;
   const infectedTwo = input.reportedCases * 50;
-  const infectedBytimeOne = infectedOne * (2 ** Math.trunc(period / 3));
-  const infectedBytimeTwo = infectedTwo * (2 ** Math.trunc(period / 3));
+  const infectedBytimeOne = infectedOne * 2 ** Math.trunc(period / 3);
+  const infectedBytimeTwo = infectedTwo * 2 ** Math.trunc(period / 3);
 
   // Challenge Two:
   const severeInfectionsBytimeOne = 0.15 * infectedBytimeOne;
   const severeInfectionsBytimeTwo = 0.15 * infectedBytimeTwo;
-  const availableBedsOne = Math.trunc((input.totalHospitalBeds * 0.35) - severeInfectionsBytimeOne);
-  const availableBedsTwo = Math.trunc((input.totalHospitalBeds * 0.35) - severeInfectionsBytimeTwo);
+  const availableBedsOne = Math.trunc(
+    input.totalHospitalBeds * 0.35 - severeInfectionsBytimeOne
+  );
+  const availableBedsTwo = Math.trunc(
+    input.totalHospitalBeds * 0.35 - severeInfectionsBytimeTwo
+  );
 
   // Challenge Three:
   // impact on ICUs
@@ -40,7 +43,9 @@ const covid19ImpactEstimator = (data) => {
   const requireVentilatorTwo = Math.trunc(infectedBytimeTwo * 0.02);
   // Impact on the Economy
   // eslint-disable-next-line max-len
-  const dollarsLostOne = Math.trunc((infectedBytimeOne * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD) / period);
+  const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = data;
+  // eslint-disable-next-line max-len
+  const dollarsLostOne = ((infectedBytimeOne * avgDailyIncomePopulation * avgDailyIncomeInUSD) / period);
   // eslint-disable-next-line max-len
   const dollarsLostTwo = Math.trunc((infectedBytimeTwo * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD) / period);
 
@@ -63,7 +68,7 @@ const covid19ImpactEstimator = (data) => {
     dollarsInFlight: dollarsLostTwo
   };
   return {
-    data: input,
+    data,
     impact,
     severeImpact
   };
