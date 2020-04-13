79e7de38025148fd5a6eddca3a63987be6cb1caa
Dominic Ngalo <otiendominic@gmail.com>
2020-04-13 21:32:29 +0300

    Fixed the logs

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const estimator = require('./src/estimator');
 
 const PORT = process.env.PORT || 3000;
 
+morgan.token('responseTime', (req) => req.responseTime);
+
 const app = express();
 const server = http.createServer(app);
 
@@ -18,18 +20,28 @@ app.use(bodyParser());
 
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, 'access.txt'),
-  { flags: 'a' }
+  { flags: 'a', encoding: 'utf8' }
 );
 // setup the logger
+
+// Writing a function that can get time response
+// eslint-disable-next-line no-use-before-define
+
+// eslint-disable-next-line no-use-before-define
+app.use(assignTime);
 app.use(
-  morgan(':method\t:url\t:status\t:response-time[0]ms\n', {
+  morgan(':method\t:url\t:status\t:responseTime\n', {
     stream: accessLogStream
   })
 );
 
-app.get('/api/v1/on-covid-19/logs', (req, res) => {
-  const data = fs.readFileSync(path.join(__dirname, './access.txt'), 'utf8');
+app.get('/api/v1/on-covid-19/logs', (req, res, next) => {
+  const data = fs.readFileSync(path.join(__dirname, './access.txt'), {
+    encoding: 'utf8',
+    'Content-Type': 'text/plain'
+  });
   res.status(200).send(data);
+  next();
 });
 // server to listen to the port
 server.listen(PORT);
@@ -37,7 +49,7 @@ server.listen(PORT);
 console.debug(`Server listening on port ${PORT}`);
 
 // Endpoint to the default request
-app.post('/api/v1/on-covid-19', (req, res) => {
+app.post('/api/v1/on-covid-19', (req, res, next) => {
   // Define the request values
   const {
     region,
@@ -65,9 +77,11 @@ app.post('/api/v1/on-covid-19', (req, res) => {
   // eslint-disable-next-line no-undef
   const doer = estimator(data);
   res.send(doer);
+
+  next();
 });
 
-app.post('/api/v1/on-covid-19/json', (req, res) => {
+app.post('/api/v1/on-covid-19/json', (req, res, next) => {
   // Define the request values
   const {
     region,
@@ -95,9 +109,10 @@ app.post('/api/v1/on-covid-19/json', (req, res) => {
   // eslint-disable-next-line no-undef
   const doer = estimator(data);
   res.json(doer);
+  next();
 });
 // Endpoint to the xml response
-app.post('/api/v1/on-covid-19/xml', (req, res) => {
+app.post('/api/v1/on-covid-19/xml', (req, res, next) => {
   // Define the request values
   const {
     region,
@@ -126,4 +141,13 @@ app.post('/api/v1/on-covid-19/xml', (req, res) => {
   const doer = estimator(data);
   res.set('Content-Type', 'application/xml');
   res.send(xml(doer));
+  next();
 });
+
+const start = new Date();
+const time = new Date() - start;
+
+function assignTime(req, res, next) {
+  req.responseTime = `${time.toString().padStart(2, '0')}ms`;
+  next();
+}
